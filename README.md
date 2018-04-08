8e9f8aa998b0f7d3317f76964e10db570fe780b5
Abudu-samuel <blvckleumas@gmail.com>
2018-04-08 20:44:47 +0100

    add test file and test script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Build Status](https://travis-ci.org/Abudu-Samuel/Testing-debugging.svg?branch=master)](https://travis-ci.org/Abudu-Samuel/Testing-debugging)
 This is a code to test for fizz buzz
---
+++ src/test/test.js
@@ -0,0 +1,33 @@
+import chai from 'chai';
+import myApp from '../main';
+
+const { assert } = chai;
+
+describe('Fizz Buzz', () => {
+  describe('Fizz Buzz test', () => {
+    it('should return Fizz for 3', () => {
+      assert.equal(myApp(3), 'Fizz');
+    });
+    it('should return 13 for 13', () => {
+      assert.equal(myApp('13'), 13);
+    });
+    it('should return Buzz for 5', () => {
+      assert.equal(myApp('5'), 'Buzz');
+    });
+    it('should return Please enter a valid number', () => {
+      assert.equal(myApp('hello'), 'Please enter a valid number');
+    });
+    it('should return Please enter a valid number greater than 0', () => {
+      assert.equal(myApp('0'), 'Please enter a valid number');
+    });
+    it('should return FizzBuzz for 15', () => {
+      assert.equal(myApp(15), 'fizzBuzz');
+    });
+    it('should return Enter number greater than 0', () => {
+      assert.equal(myApp('-1'), 'Enter number greater than 0');
+    });
+    it("should return Please enter a valid number for '' ", () => {
+      assert.equal(myApp("''"), 'Please enter a valid number');
+    });
+  });
+});
